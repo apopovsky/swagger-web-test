2dfd9719be6b57b17ae8347be64961dca694d5f0
Ariel Popovsky (Agilesight) <v-arpop@microsoft.com>
2018-03-09 18:19:58 -0300

    Add project files.

---
+++ SwaggerWeb/Controllers/ValuesController.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SwaggerWeb.Controllers
+{
+    [Route("api/{sector:sector}/[controller]")]
+    public class ValuesController : Controller
+    {
+        /// <summary>
+        /// Get all Values
+        /// </summary>
+        /// <returns></returns>
+        // GET api/values
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        /// <summary>
+        /// Get by ID
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        /// <summary>
+        /// Add new
+        /// </summary>
+        /// <param name="value"></param>
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        /// <summary>
+        /// Update
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="value"></param>
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+
+        /// <summary>
+        /// Delete
+        /// </summary>
+        /// <param name="id"></param>
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ SwaggerWeb/Startup.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace SwaggerWeb
+{
+    using System.IO;
+    using System.Reflection;
+    using Microsoft.AspNetCore.Http;
+    using Microsoft.AspNetCore.Routing;
+    using Swashbuckle.AspNetCore.Swagger;
+
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+
+            services.AddSwaggerGen(o =>
+            {
+                var fileName = GetType().GetTypeInfo().Module.Name.Replace(".dll", ".xml").Replace(".exe", ".xml");
+                o.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, fileName));
+                o.SwaggerDoc("v1", new Info { Title = "My API", Version = "v1" });
+            }).Configure<RouteOptions>(o =>
+            {
+                o.ConstraintMap.Add(SectorRouteConstraint.Key, typeof(SectorRouteConstraint));
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app
+                .UseSwagger()
+                .UseSwaggerUI(o =>
+                {
+                    o.SwaggerEndpoint(
+                        "/swagger/v1/swagger.json",
+                        "V1");
+                })
+                .UseMvc();
+        }
+    }
+
+    /// <summary>
+    /// Sector constraint
+    /// </summary>
+    public class SectorRouteConstraint:IRouteConstraint
+    {
+        public bool Match(HttpContext httpContext, IRouter route, string routeKey, RouteValueDictionary values,
+            RouteDirection routeDirection)
+        {
+            return true;
+        }
+
+        public static string Key => "sector";
+    }
+}
