20070902a77d192959bc692d76e8091ba9a2ee24
Ariel Popovsky (Agilesight) <v-arpop@microsoft.com>
2018-03-09 18:29:00 -0300

    Extra methods removed.

--- SwaggerWeb/Controllers/ValuesController.cs
+++ SwaggerWeb/Controllers/ValuesController.cs
@@ -7,58 +7,15 @@ namespace SwaggerWeb.Controllers
     public class ValuesController : Controller
     {
         /// <summary>
-        /// Get all Values
-        /// </summary>
-        /// <returns></returns>
-        // GET api/values
-        [HttpGet]
-        public IEnumerable<string> Get()
-        {
-            return new string[] { "value1", "value2" };
-        }
-
-        /// <summary>
         /// Get by ID
         /// </summary>
-        /// <param name="id"></param>
+        /// <param name="id">The value ID</param>
         /// <returns></returns>
         // GET api/values/5
         [HttpGet("{id}")]
         public string Get(int id)
         {
             return "value";
-        }
-
-        /// <summary>
-        /// Add new
-        /// </summary>
-        /// <param name="value"></param>
-        // POST api/values
-        [HttpPost]
-        public void Post([FromBody]string value)
-        {
-        }
-
-        /// <summary>
-        /// Update
-        /// </summary>
-        /// <param name="id"></param>
-        /// <param name="value"></param>
-        // PUT api/values/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody]string value)
-        {
-        }
-
-
-        /// <summary>
-        /// Delete
-        /// </summary>
-        /// <param name="id"></param>
-        // DELETE api/values/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
         }
     }
 }
